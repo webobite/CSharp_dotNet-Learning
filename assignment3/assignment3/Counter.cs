576c46562d4ce8dac56c3e0b04271b13d61fde11
webobite <singhsubham510@gmail.com>
2022-08-08 19:32:34 +0530

    Worked on assignment3 - Singletone class

---
+++ assignment3/assignment3/Counter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment3
+{
+    public class Counter
+    {
+        private static readonly Counter _counterInstance = new Counter();
+        private int count = 0;
+
+        public static Counter GetCounterInstance()
+        {
+            return _counterInstance;
+        }
+
+        public int Increment(int incrementValue)
+        {
+            return count += incrementValue;
+        }
+
+        public int Decrement(int decrementValue)
+        {
+            if(count == 0) return 0;
+            return count -= decrementValue;
+        }
+    }
+}
---
+++ assignment3/assignment3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Assignment 3
+            /**
+             * 
+                Statics, contructor
+                Write a Singleton class called Counter. It will have two functions Increment and Decrement that will increase and decrease the count respectively.
+                It has one property called count that returns the current counter. The class will have one static getinstance method to return the instance of Counter. 
+                Singleton typically means that only one instance of the class can be created.
+             */
+            Console.WriteLine("Program starting...");
+            Counter counter1 = Counter.GetCounterInstance();
+            Counter counter2 = Counter.GetCounterInstance();
+            Console.WriteLine("Increment with counter1 : " + counter1.Increment(6));
+            Console.WriteLine("Increment with counter2 : " + counter2.Increment(9));
+            Console.WriteLine("Increment with counter1 : " + counter1.Increment(9));
+            Console.WriteLine("Decrement with counter2 : " + counter2.Decrement(9));
+            Console.WriteLine("Decrement with counter1 : " + counter1.Decrement(7));
+            Console.WriteLine("Decrement with counter2 : " + counter2.Decrement(8));
+            Console.ReadLine();
+
+        }
+    }
+}
