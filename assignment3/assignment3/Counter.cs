9065d9c493f3d8b3beaa5523d7d84795ca87880e
webobite <singhsubham510@gmail.com>
2022-08-09 18:29:00 +0530

    Added the static constructor in the Counter class to intialialize the instance

--- assignment3/assignment3/Counter.cs
+++ assignment3/assignment3/Counter.cs
@@ -8,8 +8,17 @@ namespace assignment3
 {
     public class Counter
     {
-        private static readonly Counter _counterInstance = new Counter();
+        private static readonly Counter _counterInstance;
         private int count = 0;
+
+        //note : https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/static-constructors
+        // Static constructor to initialize the static variable.
+        // It is invoked before the first instance constructor is 
+        static Counter()
+        {
+            _counterInstance = new Counter();
+            Console.WriteLine("Intance initialized");
+        }
 
         public static Counter GetCounterInstance()
         {
