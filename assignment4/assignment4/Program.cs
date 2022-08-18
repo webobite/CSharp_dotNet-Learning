6e2c563b744986bc826b4d47dc75c3ce5975d2e8
webobite <singhsubham510@gmail.com>
2022-08-18 22:22:44 +0530

    Completed the assignment 4

---
+++ assignment4/assignment4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4
+{
+    public class Program
+    {
+        static public void Main(String[] args)
+        {
+            
+            Console.WriteLine("Enter the Value : ");
+            string str = Console.ReadLine();
+            string result = str.ToCurrency();
+            Console.WriteLine("RESULT :::: " + result);
+
+            /*
+            string str1 = "124";
+            string result1 = str1.ToCurrency();
+            Console.WriteLine("result1 :"+  result1);
+            
+            string str2 = "";
+            string result2 = str2.ToCurrency();
+            Console.WriteLine("result2 :" + result2);
+
+            string str3 = null;
+            string result3 = str3.ToCurrency();
+            Console.WriteLine("result3 :" + result3);
+            */
+        }
+
+    }
+}
+
---
+++ assignment4/assignment4/StringClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4
+{
+    public static class StringHelper
+    {
+        public static string ToCurrency(this string inputString)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(inputString))
+                {
+                    if(int.TryParse(inputString, out _))
+                    {
+                        return "$" + inputString;
+                    } else
+                    {
+                        throw new Exception("Input string value is not correct");
+                    }
+                } else
+                {
+                    throw new Exception("Input string is either null or empty");
+                }
+            } catch(Exception e) { throw new Exception(e.Message); }
+            
+        }
+
+    }
+}
+
